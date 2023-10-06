20aa5ce4e5d346eb65800c3e36c8244610a82930
Văn Dân <vandan250702@gmail.com>
2023-10-06 21:45:03 +0700

    update

--- src/app/members/photo-editor/photo-editor.component.ts
+++ src/app/members/photo-editor/photo-editor.component.ts
@@ -89,4 +89,16 @@ export class PhotoEditorComponent {
       }
     };
   }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe({
+      next: (_) => {
+        if (this.member) {
+          this.member.photos = this.member.photos.filter(
+            (x) => x.id === photoId
+          );
+        }
+      },
+    });
+  }
 }
